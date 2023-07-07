96628dcea029b7882cd5bca7f5bf638e2be90e59
Mauricio Cruz <mauriciogonzalolopezcruz@gmail.com>
2023-07-07 18:24:46 -0300

    se agrego el softDeletes a la tabla users y la vista principal con su ruta

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/">Principal</a>
+    <a href="/perfil">Ver perfil</a>
+    <a href="/logout">Cerrar sesión</a>
+    <a href="/login">Iniciar sesion</a>
+    <a href="/login">Registrarse</a>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,6 @@
+@include('common/header')
+<h1>Bienvenido al sitio blog</h1>
+
+
+
+@include('common/footer')
